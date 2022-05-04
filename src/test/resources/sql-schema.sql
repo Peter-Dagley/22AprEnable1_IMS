9311188be408b407ac9076ad1b30bbd35542298a
Peter-Dagley <peter_dagley@outlook.com>
2022-05-04 14:15:49 +0100

    added dummy data and tables to database

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -6,3 +6,21 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `price` DOUBLE(11, 2) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`order_id`),
+    FOREIGN KEY (`customer_id`) REFERENCES customers(`id`)
+  );
