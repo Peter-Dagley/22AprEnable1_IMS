62cc4ca5693ad85f0605d26e41f8fb31c4834af2
Peter-Dagley <peter_dagley@outlook.com>
2022-05-04 10:17:52 +0100

    added orders table to sql-schema

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -18,4 +18,12 @@ CREATE TABLE IF NOT EXISTS `ims`.`items` (
     PRIMARY KEY (`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `order_id` INT(11) DEFAULT NULL,
+    `customer_id` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`customer_id`) REFERENCES customers(`id`)
+  );
+
 
