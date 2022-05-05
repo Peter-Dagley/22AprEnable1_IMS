7b5de1963477f2b90ec684ee7d1b5e4f0ff95f8c
Peter-Dagley <peter_dagley@outlook.com>
2022-05-05 13:02:02 +0100

    added tests for item CRUD

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE IF NOT EXISTS `items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `price` DOUBLE(11, 2) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
