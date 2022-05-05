4f911540131b4d301d02a9e6faf355675fb30157
Peter-Dagley <peter_dagley@outlook.com>
2022-05-05 10:36:40 +0100

    reset test resources

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,14 +1,14 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('peter', 'dagley');
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jim', 'jimson');
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('bob', 'bobson');
+-- INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('peter', 'dagley');
+-- INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jim', 'jimson');
+-- INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('bob', 'bobson');
 
-INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('apple', '1.00');
-INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('orange', '1.50');
-INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('banana', '0.50');
-INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('kiwi', '2.00');
+-- INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('apple', '1.00');
+-- INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('orange', '1.50');
+-- INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('banana', '0.50');
+-- INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('kiwi', '2.00');
 
-INSERT INTO `ims`.`orders` (`customer_id`) VALUES (1);
-INSERT INTO `ims`.`orders` (`customer_id`) VALUES (2);
-INSERT INTO `ims`.`orders` (`customer_id`) VALUES (3);
-INSERT INTO `ims`.`orders` (`customer_id`) VALUES (4);
+-- INSERT INTO `ims`.`orders` (`customer_id`) VALUES (1);
+-- INSERT INTO `ims`.`orders` (`customer_id`) VALUES (2);
+-- INSERT INTO `ims`.`orders` (`customer_id`) VALUES (3);
+-- INSERT INTO `ims`.`orders` (`customer_id`) VALUES (4);
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -7,20 +7,20 @@ CREATE TABLE IF NOT EXISTS `customers` (
     PRIMARY KEY (`id`)
 );
 
-DROP TABLE IF EXISTS `items`;
+-- DROP TABLE IF EXISTS `items`;
 
-CREATE TABLE IF NOT EXISTS `ims`.`items` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `item_name` VARCHAR(40) DEFAULT NULL,
-    `price` DOUBLE(11, 2) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
+-- CREATE TABLE IF NOT EXISTS `ims`.`items` (
+--     `id` INT(11) NOT NULL AUTO_INCREMENT,
+--     `item_name` VARCHAR(40) DEFAULT NULL,
+--     `price` DOUBLE(11, 2) DEFAULT NULL,
+--     PRIMARY KEY (`id`)
+-- );
 
-DROP TABLE IF EXISTS `orders`;
+-- DROP TABLE IF EXISTS `orders`;
 
-CREATE TABLE IF NOT EXISTS `ims`.`orders` (
-    `order_id` INT(11) NOT NULL AUTO_INCREMENT,
-    `customer_id` INT(11) DEFAULT NULL,
-    PRIMARY KEY (`order_id`),
-    FOREIGN KEY (`customer_id`) REFERENCES customers(`id`)
-  );
+-- CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+--     `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+--     `customer_id` INT(11) DEFAULT NULL,
+--     PRIMARY KEY (`order_id`),
+--     FOREIGN KEY (`customer_id`) REFERENCES customers(`id`)
+--   );
