8d0e96e644f64528ab4c0c06d636f59bff966547
Peter-Dagley <peter_dagley@outlook.com>
2022-05-05 11:16:49 +0100

    reset sql-schema

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -6,21 +6,3 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
-
--- DROP TABLE IF EXISTS `items`;
-
--- CREATE TABLE IF NOT EXISTS `items` (
---     `id` INT(11) NOT NULL AUTO_INCREMENT,
---     `item_name` VARCHAR(40) DEFAULT NULL,
---     `price` DOUBLE(11, 2) DEFAULT NULL,
---     PRIMARY KEY (`id`)
--- );
-
--- DROP TABLE IF EXISTS `orders`;
-
--- CREATE TABLE IF NOT EXISTS `orders` (
---     `order_id` INT(11) NOT NULL AUTO_INCREMENT,
---     `customer_id` INT(11) DEFAULT NULL,
---     PRIMARY KEY (`order_id`),
---     FOREIGN KEY (`customer_id`) REFERENCES customers(`id`)
---   );
